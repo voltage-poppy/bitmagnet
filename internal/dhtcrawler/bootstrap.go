c49806572ca341371796af661d566d98d668b24c
voltage-poppy <198969844+voltage-poppy@users.noreply.github.com>
2025-06-29 05:33:06 +0800

    fix bug: get ipv6 type of netip.AddrPort if bootstrap nodes are set with ip number rather than hostname

--- internal/dhtcrawler/bootstrap.go
+++ internal/dhtcrawler/bootstrap.go
@@ -3,6 +3,8 @@ package dhtcrawler
 import (
 	"context"
 	"net"
+	"net/netip"
+	"regexp"
 	"time"
 
 	"github.com/bitmagnet-io/bitmagnet/internal/protocol/dht/ktable"
@@ -17,15 +19,28 @@ func (c *crawler) reseedBootstrapNodes(ctx context.Context) {
 			return
 		case <-time.After(interval):
 			for _, strAddr := range c.bootstrapNodes {
-				addr, err := net.ResolveUDPAddr("udp", strAddr)
-				if err != nil {
-					c.logger.Warnf("failed to resolve bootstrap node address: %s", err)
-					continue
+				matchIpNumber, _ := regexp.MatchString(`\d+\.\d+\.\d+\.\d+:\d+`, strAddr)
+				var addrPort netip.AddrPort
+				if matchIpNumber {
+					var err error
+					addrPort, err = netip.ParseAddrPort(strAddr)
+					if err != nil {
+						c.logger.Warnf("invalid IP or port number: %s", strAddr)
+						continue
+					}
+				} else {
+					addr, err := net.ResolveUDPAddr("udp", strAddr)
+					if err != nil {
+						c.logger.Warnf("failed to resolve bootstrap node address: %s", err)
+						continue
+					}
+					addrPort = addr.AddrPort()
 				}
+
 				select {
 				case <-ctx.Done():
 					return
-				case c.nodesForPing.In() <- ktable.NewNode(ktable.ID{}, addr.AddrPort()):
+				case c.nodesForPing.In() <- ktable.NewNode(ktable.ID{}, addrPort):
 					continue
 				}
 			}
